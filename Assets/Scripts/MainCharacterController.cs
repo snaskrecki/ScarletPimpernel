f54207682230404e1d06c66438a9496bc34affaa
JankowskiChristopher <72140425+JankowskiChristopher@users.noreply.github.com>
2021-03-16 19:10:39 +0100

    Basic Player
    Postać gracza działa na poruszanie się po planszy strzałkami. Na lewo od gracza na razie dodana jest niewidoczna ściana, na której gracz się zatrzymuje, bo do gracza dodany jest rigidBody2D i circleCollider.

---
+++ Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+
+    public float speed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+
+        // transform.position += new Vector3(moveInput.x * Time.deltaTime * speed, moveInput.y * Time.deltaTime * speed, 0f);
+
+        rigidBody.velocity = moveInput * speed;
+    }
+}
