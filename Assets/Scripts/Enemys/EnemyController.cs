355855888ed2a2e73f6e995d93623e158a862a7a
ciebson123 <kamil.ciebiera@gmail.com>
2021-04-15 13:51:55 +0200

    Enemy Base
    Enemy Contraller script + AI movement interface

---
+++ Assets/Scripts/Enemys/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    EnemyMovementAI moover;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moover = GetComponent<EnemyMovementAI>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = moover.Move(Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enemys/LoopMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopMovement : MonoBehaviour, EnemyMovementAI
+{
+    [SerializeField] public float speed;
+
+    public float changeTime;
+
+    float timePassed;
+
+    Vector2[] directions = { Vector2.left, Vector2.up, Vector2.right, Vector2.down };
+
+    int currDir;
+
+    public Vector2 Move(float deltaTime)
+    {
+        timePassed += deltaTime;
+        if(timePassed > changeTime)
+        {
+            timePassed = 0;
+            currDir = (currDir + 1) % directions.Length;
+        }
+        return directions[currDir] * speed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timePassed = 0;
+        currDir = 0;
+    }
+
+}
