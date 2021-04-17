be59043d479ff90eefd560a9480d088aa9080b37
Szymon Naskrecki <46072210+snaskrecki@users.noreply.github.com>
2021-04-17 02:16:04 +0200

    Add MainCameraController

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, 
+            player.position.y + offset.y, offset.z);
+    }
+}
