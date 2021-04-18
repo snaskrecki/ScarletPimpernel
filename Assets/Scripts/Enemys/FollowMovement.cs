defb9c8300d7b2bcae46026b2abcc8714d0dc072
JankowskiChristopher <72140425+JankowskiChristopher@users.noreply.github.com>
2021-04-18 09:44:44 +0200

    Updated Enemy that follows
    Dodanie nowego rodzaju ruchu - przeciwnik podąża za graczem. Jeśli gracz wejdzie w zakres przeciwnika, to przeciwnik zaczyna chodzić za nim. Troszkę musiałem zmienić MainCharacterController.

---
+++ Assets/Scripts/Enemys/FollowMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovement : MonoBehaviour, EnemyMovementAI
+{
+    public float speed;
+
+    FollowMovementObj moover;
+
+    public float range;
+
+    void Awake()
+    {
+        moover = new FollowMovementObj(speed, transform, range);
+    }
+
+    public EnemyMovementObject GetMoover() => moover;
+
+}
---
+++ Assets/Scripts/Enemys/FollowMovementObj.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovementObj : EnemyMovementObject
+{
+   
+    private float speed;
+
+    public float rangeToChasePlayer;
+
+    public Transform transform;
+
+    private bool isChasing;
+
+    public FollowMovementObj(float speed, Transform transform, float range)
+    {
+        this.speed = speed;
+        this.transform = transform;
+        this.rangeToChasePlayer = range;
+        isChasing = false;
+    }
+
+
+    // Update is called once per frame
+    public Vector2 Move(float deltaTime)
+    {
+
+        Vector2 moveDirection = Vector2.zero;
+        if (isChasing)
+        {
+            moveDirection = MainCharacterController.instance.transform.position - transform.position;
+        }
+        else if (Vector2.Distance(transform.position, MainCharacterController.instance.transform.position) < rangeToChasePlayer)
+        {
+            isChasing = true;
+        }
+
+        moveDirection.Normalize();
+
+        return moveDirection * speed;
+    }
+}
+
+
+
