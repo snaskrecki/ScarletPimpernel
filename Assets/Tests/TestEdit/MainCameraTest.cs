2c68e241916b1a49eb190ee97fbf186653326e08
Lynx <jadzia.cz21@gmail.com>
2021-04-20 20:26:31 +0200

    some minor changes in tabs, added template for camera tests

---
+++ Assets/Tests/TestEdit/MainCameraTest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MainCameraTest
+    {
+        [Test]
+        public void TestHorizontalMovement()
+        {
+            var gameObject = new GameObject();
+            var character = gameObject.AddComponent<MainCharacterController>();
+            Assert.AreEqual(
+                new Vector2(5, 0),
+                character.CalculateMovement(new Vector2(1, 0), 5, 1));
+        }
+
+        [Test]
+        public void TestVerticalMovement()
+        {
+            var gameObject = new GameObject();
+            var character = gameObject.AddComponent<MainCharacterController>();
+            Assert.AreEqual(
+                new Vector2(0, 5),
+                character.CalculateMovement(new Vector2(0, 1), 5, 1));
+        }
+
+        [Test]
+        public void TestCompositeMovement()
+        {
+            var gameObject = new GameObject();
+            var character = gameObject.AddComponent<MainCharacterController>();
+            Assert.AreEqual(
+                new Vector2(2, 2),
+                character.CalculateMovement(new Vector2(1, 1), 1, 2));
+        }
+
+    }
+}
