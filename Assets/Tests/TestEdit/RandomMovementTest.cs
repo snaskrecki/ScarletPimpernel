815c2a7eafc249205a258063f55fda77ace1a870
ciebson123 <kamil.ciebiera@gmail.com>
2021-04-21 21:52:41 +0200

    Final Polish

--- Assets/Tests/TestEdit/RandomMovementTest.cs
+++ Assets/Tests/TestEdit/RandomMovementTest.cs
@@ -12,12 +12,12 @@ namespace Tests
         [Test]
         public void RandomOneDirectionTest()
         {
-
             Vector2[] vectors = { Vector2.up };
             const int SPEED = 5;
             RandomMovementObj follower = new RandomMovementObj(1, SPEED, vectors);
             Assert.AreEqual(Vector2.up * SPEED, follower.Move(0));
         }
+
 
         [Test]
         public void ShouldMoveUpLeftTest()
@@ -37,10 +37,27 @@ namespace Tests
         }
 
         [Test]
-        public void DoNotMoveBecauseZeroTest()
+        public void DoNotMoveBecauseZeroVectorTest()
         {
             Vector2[] vectors = { Vector2.zero };
             const int SPEED = 100;
+
+            RandomMovementObj follower = new RandomMovementObj(1, SPEED, vectors);
+            Vector2 position = new Vector2(0, 0);
+            const int DURATION = 100;
+            for (int i = 0; i < DURATION; i++)
+            {
+                position += follower.Move(0);
+            }
+
+            Assert.IsTrue(position.x + position.y == 0);
+        }
+
+        [Test]
+        public void DoNotMoveBecauseZeroSpeedTest()
+        {
+            Vector2[] vectors = { Vector2.left, Vector2.right };
+            const int SPEED = 0;
 
             RandomMovementObj follower = new RandomMovementObj(1, SPEED, vectors);
             Vector2 position = new Vector2(0, 0);
@@ -62,6 +79,26 @@ namespace Tests
                                     Vector2.left + Vector2.up, Vector2.left + Vector2.down,
                                     Vector2.right + Vector2.up, Vector2.right + Vector2.down };
             const int SPEED = 50;
+
+            RandomMovementObj follower = new RandomMovementObj(1, SPEED, vectors);
+            Vector2 position = new Vector2(0, 0);
+            const int DURATION = 100000;
+            for (int i = 0; i < DURATION; i++)
+            {
+                Vector2 old_position = position;
+                position.x += Mathf.Abs(follower.Move(0).x);
+                position.y += Mathf.Abs(follower.Move(0).x);
+                Assert.IsTrue(position.x + position.y > old_position.x + old_position.y);
+            }
+        }
+
+        [Test]
+        public void MoveAllAroundTheWorldNegativeSpeedTest()
+        {
+            Vector2[] vectors = { Vector2.left, Vector2.up, Vector2.right, Vector2.down,
+                                    Vector2.left + Vector2.up, Vector2.left + Vector2.down,
+                                    Vector2.right + Vector2.up, Vector2.right + Vector2.down };
+            const int SPEED = -100;
 
             RandomMovementObj follower = new RandomMovementObj(1, SPEED, vectors);
             Vector2 position = new Vector2(0, 0);
