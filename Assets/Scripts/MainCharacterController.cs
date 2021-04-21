c1978116155ecdd66e6ee81fabcef07cbb978368
ciebson123 <kamil.ciebiera@gmail.com>
2021-04-21 14:20:21 +0200

    Cleanup

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class MainCharacterController : MonoBehaviour
 {
-
+    // speed of character, should be about 3-10
     public float speed = 5;
+
+
     private Vector2 moveInput;
 
     private Rigidbody2D rigidBody;
