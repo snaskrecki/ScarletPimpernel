358d9870f65b31fe20e81f0b131bed08a2e45029
Szymon Naskrecki <46072210+snaskrecki@users.noreply.github.com>
2021-05-11 15:33:16 +0200

    Add player attack

--- Assets/Scripts/Damage/Damagable.cs
+++ Assets/Scripts/Damage/Damagable.cs
@@ -6,6 +6,7 @@ public class Damagable : MonoBehaviour
 {
     public int maxHealth;
     int health;
+    public Animator animator;
 
     private void Awake()
     {
@@ -14,8 +15,15 @@ public class Damagable : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (animator != null)
+        {
+            animator.SetTrigger("Hurt");
+        }
         health = Mathf.Clamp(health + amount, 0, maxHealth);
-        Debug.Log(health + "/" + maxHealth);
+        if (health <= 0)
+        {
+            Die();
+        }
     }
 
     public int GetHealth() => health;
@@ -23,5 +31,10 @@ public class Damagable : MonoBehaviour
     public void ResetHealth()
     {
         health = maxHealth;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
