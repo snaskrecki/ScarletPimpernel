2e8e379a7248fbb388737e69bd04c55d5eb5e749
ciebson123 <kamil.ciebiera@gmail.com>
2021-05-20 19:40:44 +0200

    Death Animation + axis flip

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -13,18 +13,21 @@ public class MainCharacterController : MonoBehaviour
 
     public IControllerInput controllerInput;
 
+    private Animator anim;
+
+    private bool isDead = false;
     // public static MainCharacterController instance;
 
     void Awake()
     {
-        //   instance = this;
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
         if (controllerInput == null)
             controllerInput = new ControllerInput();
     }
@@ -32,14 +35,41 @@ public class MainCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = controllerInput.Horizontal;
-        moveInput.y = controllerInput.Vertical;
-        moveInput.Normalize();
+        if (isDead)
+        {
+            moveInput = Vector2.zero;
+        } else {
+            moveInput.x = controllerInput.Horizontal;
+            moveInput.y = controllerInput.Vertical;
+            moveInput.Normalize();
+            transform.localScale = SetFlip(transform.localScale, moveInput.x);
+            SetAnimation();
+        }
+    }
+
+    private void SetAnimation()
+    {
+        anim.SetFloat("Speed", moveInput.magnitude);
+    }
+
+    public Vector3 SetFlip(Vector3 vect, float x)
+    {
+        if (x < 0)
+            vect.x = -1;
+        if (x > 0)
+            vect.x = 1;
+        return vect;
     }
 
     private void FixedUpdate()
     {
         rigidBody.MovePosition(rigidBody.position + CalculateMovement(moveInput, speed, controllerInput.GetTime));
+    }
+
+    public void Die()
+    {
+        isDead = true;
+        rigidBody.simulated = false;
     }
 
     public Vector2 CalculateMovement(Vector2 coords, float speed, float time)
