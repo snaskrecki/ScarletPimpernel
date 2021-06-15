845cf67917967ddad8e73a001aa4775c0dac42c3
bajtoszek <krzybar@staszic.waw.pl>
2021-06-15 20:53:06 +0200

    Pitch alteration deleted

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -13,7 +13,6 @@ public class AudioManager : MonoBehaviour
 			s.source = gameObject.AddComponent<AudioSource>();
 			s.source.clip = s.clip;
 			s.source.volume = s.volume;
-			s.source.pitch = s.pitch;
 		}
     }
 
